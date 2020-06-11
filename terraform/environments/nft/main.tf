bf74e7e9f552e19139bb01efe2d2719e6c2e746c
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2020-06-12 00:59:19 +0100

    SFAT-191 - NFT

---
+++ terraform/environments/nft/main.tf
@@ -0,0 +1,33 @@
+#########################################################
+# Environment: NFT (Non Functional Testing)
+#
+# Deploy SCALE resources
+#########################################################
+terraform {
+  backend "s3" {
+    bucket         = "scale-terraform-state"
+    key            = "ccs-scale-infra-services-shared-nft"
+    region         = "eu-west-2"
+    dynamodb_table = "scale_terraform_state_lock"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
+}
+
+locals {
+  environment = "NFT"
+}
+
+data "aws_ssm_parameter" "aws_account_id" {
+  name = "account-id-${lower(local.environment)}"
+}
+
+module "deploy" {
+  source         = "../../modules/configs/deploy-all"
+  aws_account_id = data.aws_ssm_parameter.aws_account_id.value
+  environment    = local.environment
+}
