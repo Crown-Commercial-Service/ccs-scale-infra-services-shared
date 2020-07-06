d015159ecdbee2d5c9bed162acc7a1ab8ac41da0
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2020-07-06 14:30:07 +0100

    SFAT-251 - added BaT extensions to Agreements service

--- terraform/modules/services/agreements/ecs.tf
+++ terraform/modules/services/agreements/ecs.tf
@@ -77,7 +77,7 @@ resource "aws_ecs_task_definition" "agreements" {
     [
       {
         "name": "SCALE-EU2-${upper(var.environment)}-APP-ECS_TaskDef_Agreements",
-        "image": "${module.globals.env_accounts["mgmt"]}.dkr.ecr.eu-west-2.amazonaws.com/scale/agreements-service:34c285a-candidate",
+        "image": "${module.globals.env_accounts["mgmt"]}.dkr.ecr.eu-west-2.amazonaws.com/scale/agreements-service:5c8ea7b-candidate",
         "requires_compatibilities": "FARGATE",
         "cpu": ${var.agreements_cpu},
         "memory": ${var.agreements_memory},
