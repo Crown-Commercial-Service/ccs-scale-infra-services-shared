dcb9f6d830b857ff1ac4236c002d70acced32c87
Tom Bunting <6031257+tommyb82@users.noreply.github.com>
2021-06-15 11:35:14 +0100

    Agmts svc version 45e2345-bat-candidate (#35)

--- terraform/modules/configs/deploy-all/variables.tf
+++ terraform/modules/configs/deploy-all/variables.tf
@@ -8,7 +8,7 @@ variable "environment" {
 
 variable "ecr_image_id_agreements" {
   type    = string
-  default = "69f505d-bat-candidate"
+  default = "45e2345-bat-candidate"
 }
 
 variable "agreements_cpu" {
